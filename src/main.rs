937737e894ffa7ca4e0c7d8d34bde8ac55f147aa
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-10-23 00:00:48 +0200

    👷 build: add args reading from console

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 
-/// Search for a pattern in a file and display a line that contains it
+/// Search for a pattern in a file and display a line that contains it.
 #[derive(Parser)]
 struct Cli {
     /// The pattern to look for
@@ -10,5 +10,8 @@ struct Cli {
 }
 
 fn main() {
-    println!("Hello, world!");
+    // parse method is meant to be used in the main function
+    let args = Cli::parse();
+
+    let content = std::fs::read_to_string(&args.path).expect("Could not read the file :(");
 }
