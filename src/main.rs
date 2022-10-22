e7166627150a605e9816f8d3261443ca444ac3c4
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-10-22 23:52:34 +0200

    📝 docs: add doc comments to CLI struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use clap::Parser;
 
-/// Search for a pattern in a file and display a line that contains it.
+/// Search for a pattern in a file and display a line that contains it
 #[derive(Parser)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
+    /// A path to a file in which to search
     path: std::path::PathBuf,
 }
 
