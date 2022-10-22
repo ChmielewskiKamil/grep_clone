0af198e72f34dbd398ed6bd5d8ffa43267c3d942
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-10-22 23:51:17 +0200

    👷 build: add CLI arg struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display a line that contains it.
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
     println!("Hello, world!");
 }
