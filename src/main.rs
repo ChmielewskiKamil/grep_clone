ed4b9810c43f3a549b1bf742848fc0d2ff1ff96f
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-11-03 22:44:34 +0100

    ✨ feat: add cli args to vector, pick path and query

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
+use std::env;
 
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // args[1] is the name of the program, we don't need that
+    let search_query = &args[1];
+    let path_to_search = &args[2];
+
+    println!("Searching for: {}", search_query);
+    println!("Path to search: {}", path_to_search)
+}
