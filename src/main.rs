fe4a538870a20405a34252ade4ea23b6a06315c6
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-11-03 22:52:34 +0100

    ✨ feat: add text file handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
+    // argument handling //
+
     let args: Vec<String> = env::args().collect();
 
     // args[1] is the name of the program, we don't need that
@@ -8,5 +10,11 @@ fn main() {
     let path_to_search = &args[2];
 
     println!("Searching for: {}", search_query);
-    println!("Path to search: {}", path_to_search)
+    println!("Path to search: {}", path_to_search);
+
+    // text file handling //
+
+    let contents = fs::read_to_string(path_to_search).expect("Expected to read the file");
+
+    println!("Found following content: \n{contents}");
 }
