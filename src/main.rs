ff7a5aecb3ccb7902cff667af64bf119e0ca3bd4
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-11-04 11:58:43 +0100

    ♻️ refactor: separate args handling logic

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    // args[1] is the name of the program, we don't need that
-    let search_query = &args[1];
-    let path_to_search = &args[2];
+    let (search_query, path_to_search) = parse_config(&args);
 
     println!("Searching for: {}", search_query);
     println!("Path to search: {}", path_to_search);
@@ -17,4 +15,11 @@ fn main() {
     let contents = fs::read_to_string(path_to_search).expect("Expected to read the file");
 
     println!("Found following content: \n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let search_query = &args[1];
+    let path_to_search = &args[2];
+
+    (search_query, path_to_search)
 }
