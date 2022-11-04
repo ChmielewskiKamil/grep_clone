0e969735ea02a1ed6a3d981207a7c1979887f994
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-11-04 20:02:06 +0100

    ✨ feat: add simple search functionality

--- src/lib.rs
+++ src/lib.rs
@@ -26,3 +26,29 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("Found following content: \n{contents}");
     Ok(())
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
